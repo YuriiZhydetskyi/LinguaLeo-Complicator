1fad70e68893c06447d4071fef76c5f85048e754
Yurii.Z <zhidetskij@gmail.com>
2020-08-05 10:15:03 +0300

    Add mouse support #3

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # LinguaLeo-Complicator
 An extension for Firefox browser that makes words training process in LinguaLeo more complicated.  
-Version 1.0 just hides answers on the repetition training.  
+Now it just hides answers on the repetition training.  
 Press ArrowUp key to show answers.  
-Doesn't work for mouse using.
--- repetition_complicator.js
+++ repetition_complicator.js
@@ -1,9 +1,11 @@
-const DELAY = 350;
+const FIRST_QUESTION_DELAY = 350;
+const NEXT_QUESTION_DELAY = 600;
 const ANSVER_CLASS_NAME = "ll-Repetition__answer";
 let allAnsvers = document.getElementsByClassName(ANSVER_CLASS_NAME);
 
 //TODO: considet to do this withot timeout
-setTimeout(() => hideAnsvers(), DELAY);
+setTimeout(() => hideAnsvers(), FIRST_QUESTION_DELAY);
+setTimeout(() => setListenerForClick(), FIRST_QUESTION_DELAY * 2); // *2 is to be shure that listeners will be added
 
 document.body.addEventListener("keydown", function (event) {
   switch (event.key) {
@@ -14,10 +16,19 @@ document.body.addEventListener("keydown", function (event) {
       break;
     case 'ArrowLeft':
     case 'ArrowRight':
-      hideAnsvers();
+      setTimeout(() => hideAnsvers(), NEXT_QUESTION_DELAY);
       break;
   }
 });
+
+function setListenerForClick() {
+  for (i = 0; i < allAnsvers.length; ++i) {
+    allAnsvers[i].addEventListener("mousedown", function (event) {
+      console.log(allAnsvers.length);
+      setTimeout(() => hideAnsvers(), NEXT_QUESTION_DELAY);
+    });
+  }
+}
 
 function hideAnsvers() {
   for (i = 0; i < allAnsvers.length; ++i) {
